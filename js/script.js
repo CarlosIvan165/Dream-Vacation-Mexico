03dcb49a85eb98ab72ea3eecb88fb2cc049060ed
CarlosIvan165 <ccarlos5412014@outlook.com>
2024-08-13 11:41:13 -0600

    carousel
    Se agrego carousel para certificaciones

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 'use strict';
 
+document.addEventListener('DOMContentLoaded', function(){
+    iniciarApp();
+});
+
+
+function iniciarApp(){
+    crearGaleria('certifications-michelle', 8, );
+    /* agregarEventosDeFiltro(); */
+}
+
+//Galeria para portafolio
+function crearGaleria(categoria, cantidad, puesto) {
+    const galeria = document.querySelector(`.galeria-imagenes`);
+    
+    for(let i = 1; i <= cantidad; i++){
+        const imagen = document.createElement('li');
+        imagen.classList.add('project-item');
+        imagen.classList.add('active');
+        imagen.dataset.filterItem ='';
+        imagen.dataset.category = categoria.toLowerCase();
+        imagen.innerHTML = `
+            <a class="project-box" href="#">
+                <figure class="project-img">
+                    <div class="project-item-icon-box">
+                        <ion-icon name="eye-outline" role="img" class="md hydrated" aria-label="eye outline"></ion-icon>
+                    </div>
+                    <img loading="lazy" class="img-thumb" src="img/${categoria = "certifications-michelle"}/${i}.png" alt="imagen galeria">
+                </figure>
+                <h3 class="project-title">${categoria}</h3>
+            </a>
+        `;
+        
+        imagen.onclick = function() {
+            mostrarImagen(categoria, i);
+        };
+
+        /* galeria.appendChild(imagen); */
+    }
+
+    // Redefinir filterItems después de crear las imágenes
+    /* agregarEventosDeFiltro(); */
+}
+
 // page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
